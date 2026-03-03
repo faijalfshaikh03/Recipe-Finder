3001b8bb6b3cdfe11cc0b8a02b36c99608bb1def
faijalfshaikh <faijalfshaikh@gmail.com>
2026-03-03 16:42:08 +0530

    Finished Database Configuration

--- backend/db/schema.js
+++ backend/db/schema.js
@@ -2,13 +2,12 @@ import { pgTable, serial, text, timestamp, integer  } from "drizzle-orm/pg-core"
 
 export const favouritesTable = pgTable("favourites", {
     id: serial("id").primaryKey(),
-    userID: text("user_id").notNull(),
+    userId: text("user_id").notNull(),
     recipeId: integer("recipe_id").notNull(),
     title: text("title").notNull(),
     image: text("image"),
     cookTime: text("cook_time"),
     servings: text("servings"),
     createdAt: timestamp("created_at").defaultNow()
-
 }
 )
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,41 @@
 import express from "express"
+import { db } from "./db/db.js"
+import { favouritesTable } from "./db/schema.js"
+import "dotenv/config"
 
 
 const app = express()
+app.use(express.json());
 const PORT = process.env.PORT || 8000
 
 app.get('/api/health', (req,res) => {
     res.status(200).json({success: true})
+})
+
+app.post('/api/favorites', async (req,res) => {
+    try {
+        const {userId, recipeId, title, image, cookTime, servings} = req.body;
+
+        if(!userId || !recipeId || !title) {
+            res.status(400).json({error: "Missing required fields"})
+        }
+
+        console.log(req.body)
+        const newFavorite = await db.insert(favouritesTable).values({
+            userId,
+            recipeId,
+            title,
+            image,
+            cookTime,
+            servings
+        }).returning()
+
+        res.status(201).json(newFavorite[0])
+
+    } catch (error) {
+        console.log("Some error occured ",error)
+        return res.status(400).json({message: "Some error occured"});
+    }
 })
 
 app.listen(PORT, () => {
